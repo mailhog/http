f0ff7c573e715d0d01c8bd5f1478270cba0ffe1c
Ian Kent <email@iankent.co.uk>
2015-04-20 14:39:29 +0100

    Fix mailhog/MailHog#47 - forgotten commit

--- server.go
+++ server.go
@@ -1,20 +1,21 @@
 package http
 
 import (
+	"net/http"
+
+	"github.com/gorilla/pat"
 	"github.com/ian-kent/go-log/log"
-	gotcha "github.com/ian-kent/gotcha/app"
 )
 
-func Listen(httpBindAddr string, Asset func(string) ([]byte, error), exitCh chan int, registerCallback func(*gotcha.App)) {
+// Listen binds to httpBindAddr
+func Listen(httpBindAddr string, Asset func(string) ([]byte, error), exitCh chan int, registerCallback func(http.Handler)) {
 	log.Info("[HTTP] Binding to address: %s", httpBindAddr)
 
-	var app = gotcha.Create(Asset)
-	app.Config.Listen = httpBindAddr
+	pat := pat.New()
+	registerCallback(pat)
 
-	registerCallback(app)
-
-	app.Start()
-
-	<-make(chan int)
-	exitCh <- 1
+	err := http.ListenAndServe(httpBindAddr, pat)
+	if err != nil {
+		log.Fatalf("[HTTP] Error binding to address %s: %s", httpBindAddr, err)
+	}
 }
